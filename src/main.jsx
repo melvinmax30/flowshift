3fa9671447770bdc74b51b0d7abdd8d37bade116
Melvin Deva <melvin_max10@hotmail.com>
2025-05-06 14:44:47 +0800

    updated Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Make sure this points to your App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SubscribePage from './pages/Subscribe';
+import { SuccessPage } from './pages/Success';
+import { CancelPage } from './pages/Cancel';
+import AdminPanel from './admin/AdminPanel';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SubscribePage />} />
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
