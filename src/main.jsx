ad2ef31230c7bccd2a0b9f785fed6e6ef5240421
Melvin Deva <melvin_max10@hotmail.com>
2025-05-06 14:41:09 +0800

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Make sure this points to your App.jsx
-import './index.css'; // Or your Tailwind/global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
