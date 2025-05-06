d38caa6b2bd77848c2cf9bf75bc7c96fd8607591
Melvin Deva <melvin_max10@hotmail.com>
2025-05-06 14:05:36 +0800

    Initial commit for Flowshift Vite React project

---
+++ src/pages/SuccessPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function SuccessPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1 style={{ color: 'green' }}>✅ Subscription Successful</h1>
+      <p>Thank you for subscribing to FlowShift. You now have full access!</p>
+    </div>
+  );
+}
